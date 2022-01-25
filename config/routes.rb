841af6c1bc645f8dad37f0670df0975c9b6ae76d
JM Arenas <rns.jey@gmail.com>
2022-01-25 19:03:05 +0800

    Reverting to cf54d2c

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  
   devise_for :users
-  resources :groups
-  #resources :users
-  
-  root 'home#index'
-  # root 'groups#index'
-  
+
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
