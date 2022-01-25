fd5178ded85e82eec275ba3641fe6219b23e9ab0
Kimcarl Cacas <kimcarlcacas@yahoo.com>
2022-01-25 19:36:28 +0800

    scaffolds Posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_24_114824) do
+ActiveRecord::Schema.define(version: 2022_01_25_113518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 2022_01_24_114824) do
     t.uuid "user_id"
     t.string "name"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "content"
+    t.uuid "group_id"
+    t.uuid "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
