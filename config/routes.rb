990573cc62ba51ce20445d9781117a7bdc3f2d1e
JM Arenas <rns.jey@gmail.com>
2022-01-25 10:55:18 +0800

    Generate home controller to check tailwind functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users
 
-  devise_scope :user do
-    root to: 'devise/sessions#new'
-  end
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
