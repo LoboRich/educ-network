d5615541e91e26f26479aeb517977c6de598044f
JM Arenas <rns.jey@gmail.com>
2022-01-26 20:07:49 +0800

    Set default root pages for authenticated and non-authenticated users

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  get 'home/index'
   devise_for :users
 
-  root to: 'home#index'
+  devise_scope :user do
+    authenticated :user do
+      namespace :users do
+        get 'home/index', as: :authenticated_root
+      end
+    end
 
-  # devise_scope :user do
-  #   root to: 'devise/sessions#new'
-  # end
+    root to: 'devise/sessions#new'
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
