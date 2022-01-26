831f1bc71f25c82760ab16ccdb369659443115cc
idlehands08 <geofftd08@gmail.com>
2022-01-26 20:31:18 +0800

    sidebar initial layout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,8 @@ require("channels");
 
 import "../stylesheets/application"
 
+// import fontawesome css.all so fontawesome icons can be loaded onto the web page 
+import "@fortawesome/fontawesome-free/css/all"
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
