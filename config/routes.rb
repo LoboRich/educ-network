88e951c90c03c67a3931e6bb90aa357a328e77e9
JM Arenas <rns.jey@gmail.com>
2022-01-26 19:35:55 +0800

    Temporary homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   get 'home/index'
   devise_for :users
 
-  devise_scope :user do
-    root to: 'devise/sessions#new'
-  end
+  root to: 'home#index'
+
+  # devise_scope :user do
+  #   root to: 'devise/sessions#new'
+  # end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
