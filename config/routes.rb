041543ba8568728c7b0031a9a6bc7d8606884c0f
JM Arenas <rns.jey@gmail.com>
2022-01-26 20:58:20 +0800

    Rebuilt homepage as welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users
 
-  devise_scope :user do
-    authenticated :user do
-      namespace :users do
-        get 'home/index', as: :authenticated_root
-      end
-    end
+  # devise_scope :user do
+  #   authenticated :user do
+  #     namespace :users do
+  #       , as: :authenticated_root
+  #     end
+  #   end
 
-    root to: 'devise/sessions#new'
-  end
+  #   root to: 'devise/sessions#new'
+  # end
+
+  root to: 'home#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
