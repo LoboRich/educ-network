c89c7b541fb97aaff1852b7ac5079038009714d6
Kimcarl Cacas <kimcarlcacas@yahoo.com>
2022-02-02 17:22:24 +0800

    add authentication before action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
-
+  before_action :authenticate_user!
   # GET /posts or /posts.json
   def index
     @posts = Post.all
@@ -8,6 +8,7 @@ class PostsController < ApplicationController
 
   # GET /posts/1 or /posts/1.json
   def show
+    @comments  = @post.comments.where.not(id: nil)
   end
 
   # GET /posts/new
