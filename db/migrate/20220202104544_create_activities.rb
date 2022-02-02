acf39eb1ed0b311a812590f4f0a767d4c147158a
JM Arenas <rns.jey@gmail.com>
2022-02-02 18:57:52 +0800

    Built activities table structure and db migrate

--- db/migrate/20220202104544_create_activities.rb
+++ db/migrate/20220202104544_create_activities.rb
@@ -1,6 +1,8 @@
 class CreateActivities < ActiveRecord::Migration[6.0]
   def change
     create_table :activities, id: :uuid do |t|
+      t.string :title
+      t.string :instructions
 
       t.timestamps
     end
