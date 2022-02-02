cf23aeffce3f5146b393a9fae7552699240bae3c
Kimcarl Cacas <kimcarlcacas@yahoo.com>
2022-02-02 16:58:02 +0800

    adds relationship between posts and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :groups
   has_many :class_students
+  has_many :posts
   
   def fullname
     "#{self.first_name} #{self.middle_name} #{self.last_name}"
