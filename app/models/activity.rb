1e607fbcbd1687ba89fa6928f2116efa78e7d06b
JM Arenas <rns.jey@gmail.com>
2022-02-08 19:32:15 +0800

    Built activity and questions association and accepts nested attributes

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,5 +1,7 @@
 class Activity < ApplicationRecord
   belongs_to :group
+  has_many :questions, inverse_of: :activity
+  accepts_nested_attributes_for :questions
 
   validates :title, presence: true
 end
