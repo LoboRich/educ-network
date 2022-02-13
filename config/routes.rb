0a8df9a61aae43e9b285d6670748534351ff324d
Rich Lobo <lobo.r0018@gmail.com>
2022-02-13 23:00:23 +0800

    Set group index page to be a root directory

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
     end
   end
 
-  root :to => "profiles#index"
+  root :to => "groups#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'classpage', to: 'html_pages#classpage'
 end
