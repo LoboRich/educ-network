627cd772fe6863351b329909b91d71f8552c5978
idlehands08 <geofftd08@gmail.com>
2022-02-17 02:35:07 +0800

    assignments model testing

--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -11,5 +11,12 @@ RSpec.describe Assignment, :type => :model do
     expect(group).to be_valid
   end
 
-  
+  it "is not valid without a title " do
+    # user = User.create(email: "johndoe@example.com", password: "pass1234", role: "teacher", first_name: "John", middle_name: "Smith", last_name: "Doe")
+    # user.confirm
+    group = Group.create(user_id: user.id, name: "TestGroup", description:"Hello World")
+    assignment = Assignment.create(group_id: group.id, title: nil, instructions:"Hello World")
+    expect(group).to be_valid
+  end
+
 end
