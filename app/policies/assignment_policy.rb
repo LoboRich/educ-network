909979de37e3342c72c35a51a3085653ea85c43d
Rich Lobo <lobo.r0018@gmail.com>
2022-02-16 19:43:03 +0800

    Limit creation/updating of assignment to teacher only

---
+++ app/policies/assignment_policy.rb
@@ -0,0 +1,40 @@
+class AssignmentPolicy < ApplicationPolicy
+    attr_reader :user, :record
+  
+    def initialize(user, record)
+      @user = user
+      @record = record
+    end
+
+    def new?
+      [
+        'teacher'
+      ].include?(user.role)
+    end
+
+    def edit?
+      [
+        'teacher'
+      ].include?(user.role)
+    end
+
+    def actions?
+      [
+        'teacher'
+      ].include?(user.role)
+    end
+    
+    class Scope
+      attr_reader :user, :scope
+  
+      def initialize(user, scope)
+        @user = user
+        @scope = scope
+      end
+  
+      def resolve
+        scope.all
+      end
+    end
+  end
+    
