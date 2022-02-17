a22460828d3f77af3450d517e18cac043b89511b
idlehands08 <geofftd08@gmail.com>
2022-02-17 15:45:28 +0800

    assignment submission policy

---
+++ app/policies/assignment_submission_policy.rb
@@ -0,0 +1,33 @@
+class AssignmentPolicy < ApplicationPolicy
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def new?
+    [
+      'student'
+    ].include?(user.role)
+  end
+
+  def edit?
+    [
+      'teacher'
+    ].include?(user.role)
+  end
+
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      scope.all
+    end
+  end
+end
