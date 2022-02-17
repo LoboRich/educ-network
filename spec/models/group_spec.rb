5cc907160f68d0fef801df1b47338ae34b524978
Rich Lobo <lobo.r0018@gmail.com>
2022-02-17 10:53:01 +0800

    Add test and model validation for possible duplication of student in a group

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,12 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Group, :type => :model do
-  let(:user) { create(:user) }
+  let(:teacher) { create(:user) }
 
   it "is valid with the required details" do
-    # user = User.create(email: "johndoe@example.com", password: "pass1234", role: "teacher", first_name: "John", middle_name: "Smith", last_name: "Doe")
-    # user.confirm
-    group = Group.new(user_id: user.id, name: "TestGroup", description:"Hello World")
+    group = Group.new(user_id: teacher.id, name: "TestGroup", description:"Hello World")
     expect(group).to be_valid
   end
 
@@ -16,13 +14,21 @@ RSpec.describe Group, :type => :model do
   end
 
   it "is not valid without a name" do
-    group = Group.new(user_id: user.id, name: nil, description: "Hello World")
+    group = Group.new(user_id: teacher.id, name: nil, description: "Hello World")
     expect(group).to_not be_valid
   end
 
   it "is not valid without a description"do
-    group = Group.new(user_id: user.id, name: "Class Masipag")
+    group = Group.new(user_id: teacher.id, name: "Class Masipag")
     expect(group).to_not be_valid
+  end
+
+  it "has no duplicate student record" do
+    student = User.create(email: "student@er.com", password: "password", role: "student")
+    group = Group.new(user_id: teacher.id, name: "TestSample", description:"Hello World")
+    class_student1 = ClassStudent.new(user_id: student.id, group_id: group.id)
+    class_student2 = ClassStudent.new(user_id: student.id, group_id: group.id)
+    expect(class_student2).to_not be_valid
   end
   
 end
